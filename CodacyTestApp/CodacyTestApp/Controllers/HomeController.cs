e966f6638856e161ad427ddbc83e79e4e10628dd
Alen Adanić <alen.adanic@king-ict.hr>
2019-08-22 15:45:51 +0200

    new-test-commit

--- CodacyTestApp/CodacyTestApp/Controllers/HomeController.cs
+++ CodacyTestApp/CodacyTestApp/Controllers/HomeController.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
 namespace CodacyTestApp.Controllers
 {
@@ -25,6 +21,14 @@ namespace CodacyTestApp.Controllers
 			ViewBag.Message = "Your contact page.";
 
 			return View();
+		}
+
+
+		private void NewPrivateMethod()
+		{
+			/*
+			  NOP
+			 */
 		}
 	}
 }

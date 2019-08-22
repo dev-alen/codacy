e8fbca150c42b61cc289e16a5ac8118e27e3caae
DEV\alen.adanic <alen.adanic@king-ict.hr>
2019-08-22 15:05:26 +0200

    initial-commit

---
+++ CodacyTestApp/CodacyTestApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CodacyTestApp.MvcApplication" Language="C#" %>
---
+++ CodacyTestApp/CodacyTestApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CodacyTestApp.Startup))]
+namespace CodacyTestApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}

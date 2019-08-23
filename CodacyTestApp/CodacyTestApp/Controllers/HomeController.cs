66964eb4b804510c3573dc111f639a62e3e6a4cc
DEV\alen.adanic <alen.adanic@king-ict.hr>
2019-08-23 09:35:29 +0200

    duplication

--- CodacyTestApp/CodacyTestApp/Controllers/HomeController.cs
+++ CodacyTestApp/CodacyTestApp/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using System.Web.Mvc;
+﻿using System.Collections.Generic;
+using System.Text;
+using System.Web.Mvc;
 
 namespace CodacyTestApp.Controllers
 {
@@ -27,6 +29,25 @@ namespace CodacyTestApp.Controllers
 		private void NewPrivateMethod()
 		{
 			// Deleted comment
+			// test duplication
+			List<int> list = new List<int>() { 1, 2, 3, 4, 5 };
+			StringBuilder sb = new StringBuilder();
+			foreach (var item in list)
+			{
+				sb.Append(item);
+			}
+		}
+
+		private void NewPrivateMethodMethod()
+		{
+			// Deleted comment
+			// test duplication
+			List<int> list = new List<int>() { 1, 2, 3, 4, 5 };
+			StringBuilder sb = new StringBuilder();
+			foreach (var item in list)
+			{
+				sb.Append(item);
+			}
 		}
 	}
 }
